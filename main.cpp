0f1b46f68bded53f55e278d873baf61a5ba944c0
sandeepmaddula <saisandeepmaddula2@gmail.com>
2020-05-15 18:57:37 +0200

    Added Descriptors

--- main.cpp
+++ main.cpp
@@ -243,18 +243,133 @@ void detKeypoints()
 
 }
 
-void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> &kPtsSource, vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef, vector<cv::DMatch> &matches, string descriptorType, string matcherType, string selectorType)
+void descKeypoints1()
 {
+	// load image from file and convert to grayscale
+	cv::Mat imgGray;
+	cv::Mat img = cv::imread("C:\\Users\\SandeepMaddula\\source\\repos\\opencv\\opencv\\img1.png");
+	cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+
+	// BRISK detector / descriptor
+	cv::Ptr<cv::FeatureDetector> detector = cv::BRISK::create();
+	vector<cv::KeyPoint> kptsBRISK;
+
+	double t = (double)cv::getTickCount();
+	detector->detect(imgGray, kptsBRISK);
+	t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+	cout << "BRISK detector with n= " << kptsBRISK.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+	cv::Ptr<cv::DescriptorExtractor> descriptor = cv::BRISK::create();
+	cv::Mat descBRISK;
+	t = (double)cv::getTickCount();
+	descriptor->compute(imgGray, kptsBRISK, descBRISK);
+	t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+	cout << "BRISK descriptor in " << 1000 * t / 1.0 << " ms" << endl;
+
+	// visualize results
+	cv::Mat visImage = img.clone();
+	cv::drawKeypoints(img, kptsBRISK, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	string windowName = "BRISK Results";
+	cv::namedWindow(windowName, 1);
+	imshow(windowName, visImage);
+	cv::waitKey(0);
+
+
+	//detector = cv::xfeatures2d::SIFT::create(); 
+	//vector<cv::KeyPoint> kptsSIFT;
+
+	//t = (double)cv::getTickCount();
+	//detector->detect(imgGray, kptsSIFT);
+	//t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+	//cout << "SIFT detector with n= " << kptsSIFT.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+	//descriptor = cv::xfeatures2d::SiftDescriptorExtractor::create();
+	//cv::Mat descSIFT;
+	//t = (double)cv::getTickCount();
+	//descriptor->compute(imgGray, kptsSIFT, descSIFT);
+	//t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+	//cout << "SIFT descriptor in " << 1000 * t / 1.0 << " ms" << endl;
+
+	//visImage = img.clone();
+	//cv::drawKeypoints(img, kptsSIFT, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	//windowName = "SIFT Results";
+	//cv::namedWindow(windowName, 2);
+	//imshow(windowName, visImage);
+	//cv::waitKey(0);
+}
+
+
+void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> &kPtsSource, vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef,
+	vector<cv::DMatch> &matches, string descriptorType, string matcherType, string selectorType)
+{
+
+	// configure matcher
 	bool crossCheck = false;
 	cv::Ptr<cv::DescriptorMatcher> matcher;
-	
+
 	if (matcherType.compare("MAT_BF") == 0)
 	{
+
 		int normType = descriptorType.compare("DES_BINARY") == 0 ? cv::NORM_HAMMING : cv::NORM_L2;
 		matcher = cv::BFMatcher::create(normType, crossCheck);
-		cout << "BF matching =" << crossCheck;
+		cout << "BF matching cross-check=" << crossCheck;
 	}
+	else if (matcherType.compare("MAT_FLANN") == 0)
+	{
+		if (descSource.type() != CV_32F)
+		{ // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
+			descSource.convertTo(descSource, CV_32F);
+			descRef.convertTo(descRef, CV_32F);
+		}
+
+		matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
+		cout << "FLANN matching";
+	}
+
+	// perform matching task=
+	if (selectorType.compare("SEL_NN") == 0)
+	{ // nearest neighbor (best match)
+
+		double t = (double)cv::getTickCount();
+		matcher->match(descSource, descRef, matches); // Finds the best match for each descriptor in desc1
+		t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+		cout << " (NN) with n=" << matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
+	}
+	else if (selectorType.compare("SEL_KNN") == 0)
+	{ // k nearest neighbors (k=2)
+
+		vector<vector<cv::DMatch>> knn_matches;
+		double t = (double)cv::getTickCount();
+		matcher->knnMatch(descSource, descRef, knn_matches, 2); // finds the 2 best matches
+		t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+		cout << " (KNN) with n=" << knn_matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
+
+		// STUDENT TASK
+		// filter matches using descriptor distance ratio test
+		double minDescDistRatio = 0.8;
+		for (auto it = knn_matches.begin(); it != knn_matches.end(); ++it)
+		{
+
+			if ((*it)[0].distance < minDescDistRatio * (*it)[1].distance)
+			{
+				matches.push_back((*it)[0]);
+			}
+		}
+		cout << "# keypoints removed = " << knn_matches.size() - matches.size() << endl;
+	
+	}
+
+	// visualize results
+	cv::Mat matchImg = imgRef.clone();
+	cv::drawMatches(imgSource, kPtsSource, imgRef, kPtsRef, matches,
+		matchImg, cv::Scalar::all(-1), cv::Scalar::all(-1), vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+	string windowName = "Matching keypoints between two camera images (best 50)";
+	cv::namedWindow(windowName, 7);
+	cv::imshow(windowName, matchImg);
+	cv::waitKey(0);
 }
+
 
 
 
@@ -263,6 +378,24 @@ int main()
 	//gaussian_smoothing();
 	//magnitude_sobel();
 	//cornernessHarris();
-	detKeypoints();
-	
+	//detKeypoints();
+	/*Mat imgSource = imread("C:\\Users\\SandeepMaddula\\source\\repos\\opencv\\opencv\\img1gray.png");
+	Mat imgRef =  imread("C:\\Users\\SandeepMaddula\\source\\repos\\opencv\\opencv\\img2gray.png");
+
+	vector<cv::KeyPoint> kptsSource, kptsRef;
+	readKeypoints("C:\\Users\\SandeepMaddula\\source\\repos\\opencv\\opencv\\C35A5_KptsSource_BRISK_large.dat", kptsSource);
+	readKeypoints("C:\\Users\\SandeepMaddula\\source\\repos\\opencv\\opencv\\C35A5_KptsRef_BRISK_large.dat", kptsRef);
+
+	Mat descSource, descRef;
+	readDescriptors("C:\\Users\\SandeepMaddula\\source\\repos\\opencv\\opencv\\C35A5_DescSource_BRISK_large.dat", descSource);
+	readDescriptors("C:\\Users\\SandeepMaddula\\source\\repos\\opencv\\opencv\\C35A5_DescSource_BRISK_large.dat", descRef);
+
+	vector<DMatch> matches;
+
+	string matcherType = "MAT_BF";
+	string descriptorType = "DES_BINARY";
+	string selectorType = "SEL_NN";
+	matchDescriptors(imgSource, imgRef, kptsSource, kptsRef, descSource, descRef, matches, descriptorType, matcherType, selectorType);
+*/
+	descKeypoints1();
 }
